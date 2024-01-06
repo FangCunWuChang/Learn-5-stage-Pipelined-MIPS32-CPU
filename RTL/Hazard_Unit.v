6196609ca442d20c1cd329a687dcb8353b87f762
WuChang <3142324836@qq.com>
2024-01-06 16:26:23 +0800

    Update Hazard_Unit

--- RTL/Hazard_Unit.v
+++ RTL/Hazard_Unit.v
@@ -7,21 +7,21 @@ module Hazard_Unit (
 );
 
 wire  lwStall;
-wire  branshStall;
+wire  branchStall;
 
 assign  lwStall      = (((rsD == rtE) | (rtD == rtE)) & memToRegE);     //执行数据与译码数据相关
-assign  branshStall  = ((branchD & regWriteE & ((writeRegE == rsD) | (writeRegE == rtD))) | (branchD & memToRegM & ((writeRegM == rsD) | (writeRegM == rtD)))); //执行数据与译码数据或访存数据与译码数据相关
-assign  stallF       = (lwStall | branshStall);
-assign  stallD       = (lwStall | branshStall);
-assign  flushE       = (lwStall | branshStall | jumpD);
+assign  branchStall  = ((branchD & regWriteE & ((writeRegE == rsD) | (writeRegE == rtD))) | (branchD & memToRegM & ((writeRegM == rsD) | (writeRegM == rtD)))); //执行数据与译码数据或访存数据与译码数据相关
+assign  stallF       = (lwStall | branchStall);
+assign  stallD       = (lwStall | branchStall);
+assign  flushE       = (lwStall | branchStall | jumpD);
 assign  forwardAD    = ((rsD != 0) & (rsD == writeRegM) & regWriteM);   //访存数据与译码数据相关
 assign  forwardBD    = ((rtD != 0) & (rtD == writeRegM) & regWriteM);   //访存数据与译码数据相关
 
 always @(*)
     begin
-        if ((rsE != 0) & (rsE == writeRegM) & regWriteM)
+        if ((rsE != 0) & (rsE == writeRegM) & regWriteM)                //执行数据与访存数据相关
                 forwardAE = 2'b10;
-        else if ((rsE != 0) & (rsE == writeRegW) & regWriteW)
+        else if ((rsE != 0) & (rsE == writeRegW) & regWriteW)           //执行数据与回写数据相关
                 forwardAE = 2'b01;
         else
                 forwardAE = 2'b00;
@@ -29,9 +29,9 @@ always @(*)
 
 always @(*)
     begin
-        if ((rtE != 0) & (rtE == writeRegM) & regWriteM)
+        if ((rtE != 0) & (rtE == writeRegM) & regWriteM)                //执行数据与访存数据相关
                 forwardBE = 2'b10;
-        else if ((rtE != 0) & (rtE == writeRegW) & regWriteW)
+        else if ((rtE != 0) & (rtE == writeRegW) & regWriteW)           //执行数据与回写数据相关
                 forwardBE = 2'b01;
         else
                 forwardBE = 2'b00;
